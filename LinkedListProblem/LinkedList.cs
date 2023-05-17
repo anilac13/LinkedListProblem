652208039e0a54cbded3ccf92702544f5787a055
anilac13 <anil4u2496@gmail.com>
2023-05-17 21:17:33 +0530

    [Anil] Add:Create and add 56,30,70 to Linked list

---
+++ LinkedListProblem/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    public class LinkedList
+    {
+        private Node head;
+        public void AddToLast(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        public void Display()
+        {
+            Node temp = head;
+            if (head == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            Console.Write("Elements in Linked list are: ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+
+namespace LinkedListProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.AddToLast(56);
+            linkedList.AddToLast(30);
+            linkedList.AddToLast(70);
+            linkedList.Display();
+        }
+    }
+}
